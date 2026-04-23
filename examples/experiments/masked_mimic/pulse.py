fix: Fix PULSE KL annealing schedule in pulse.py and pulse_bm.py

examples/experiments/masked_mimic/pulse.py
@@ -245,7 +245,7 @@ def agent_config(
             prior_regu_weight=0.005,
             prior_mean_regu_coeff=0.001,
             prior_logvar_regu_coeff=0.001,
-            kld_schedule=KLDScheduleConfig(start_epoch=2500, end_epoch=5000, init_kld_coeff=0.001, end_kld_coeff=0.01),
+            kld_schedule=KLDScheduleConfig(start_epoch=2500, end_epoch=5000, init_kld_coeff=0.01, end_kld_coeff=0.001),
         ),
         optimizer=OptimizerConfig(_target_="torch.optim.Adam", lr=2e-5),
     )

examples/experiments/masked_mimic/pulse_bm.py
@@ -354,8 +354,8 @@ def agent_config(
             kld_schedule=KLDScheduleConfig(
                 start_epoch=2500,
                 end_epoch=5000,
-                init_kld_coeff=0.001,
-                end_kld_coeff=0.01,
+                init_kld_coeff=0.01,
+                end_kld_coeff=0.001,
             ),
         ),
         optimizer=OptimizerConfig(_target_="torch.optim.Adam", lr=2e-5),