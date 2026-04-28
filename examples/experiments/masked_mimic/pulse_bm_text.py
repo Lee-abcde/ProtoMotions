2e64de0d65b9ca9ced9234c110449c1e7d376859
vlg-g1 <ljy1340691470@gmail.com>
2026-04-29 00:24:58 +0200

    dev: Add text-conditioned PULSE BM baseline

---
+++ examples/experiments/masked_mimic/pulse_bm_text.py
@@ -0,0 +1,512 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 The ProtoMotions Developers
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Text-conditioned PULSE BM baseline for comparing against phase embeddings."""
+
+import argparse
+
+from protomotions.robot_configs.base import RobotConfig
+from protomotions.simulator.base_simulator.config import (
+    SimulatorConfig,
+    ActionNoiseDomainRandomizationConfig,
+    FrictionDomainRandomizationConfig,
+    CenterOfMassDomainRandomizationConfig,
+    RobotNoiseConfig,
+    PushDomainRandomizationConfig,
+    DomainRandomizationConfig,
+)
+from protomotions.components.terrains.config import (
+    TerrainConfig,
+    TerrainSimConfig,
+    CombineMode,
+)
+from protomotions.envs.base_env.config import EnvConfig
+from protomotions.components.scene_lib import SceneLibConfig
+from protomotions.components.motion_lib import MotionLibConfig
+from protomotions.agents.distill.config import (
+    KLDScheduleConfig,
+    DistillAgentConfig,
+    DistillModelConfig,
+    VaeConfig,
+    VaeNoiseType,
+)
+
+
+def additional_experiment_arguments(parser: argparse.ArgumentParser):
+    parser.add_argument(
+        "--expert-model-path",
+        type=str,
+        default=None,
+        help="Path to expert model checkpoint for distillation training",
+    )
+
+
+def terrain_config(args: argparse.Namespace):
+    return TerrainConfig(
+        sim_config=TerrainSimConfig(
+            static_friction=1,
+            dynamic_friction=1,
+            restitution=0.0,
+            combine_mode=CombineMode.MULTIPLY,
+        )
+    )
+
+
+def scene_lib_config(args: argparse.Namespace):
+    scene_file = args.scenes_file if hasattr(args, "scenes_file") else None
+    return SceneLibConfig(scene_file=scene_file)
+
+
+def motion_lib_config(args: argparse.Namespace):
+    return MotionLibConfig(motion_file=args.motion_file)
+
+
+def env_config(robot_cfg: RobotConfig, args: argparse.Namespace) -> EnvConfig:
+    from protomotions.envs.motion_manager.config import MimicMotionManagerConfig
+    from protomotions.envs.control.mimic_control import MimicControlConfig
+    from protomotions.envs.mdp_component import MdpComponent
+    from protomotions.envs.context_views import EnvContext
+    from protomotions.envs.component_factories import (
+        reduced_coords_obs_factory,
+        mimic_target_poses_reduced_coords_factory,
+        max_coords_obs_factory,
+        mimic_target_poses_max_coords_factory,
+        previous_actions_factory,
+        action_smoothness_factory,
+        global_anchor_ori_rew_factory,
+        relative_body_pos_rew_factory,
+        relative_body_ori_rew_factory,
+        global_body_lin_vel_rew_factory,
+        global_body_ang_vel_rew_factory,
+        anchor_height_error_term_factory,
+    )
+    from protomotions.envs.rewards import compute_soft_pos_limit_rew
+    from protomotions.envs.action import make_bm_pd_action_config
+    from protomotions.envs.obs.vq_pae_bm import passthrough_text_embedding
+
+    control_components = {
+        "mimic": MimicControlConfig(
+            bootstrap_on_episode_end=True,
+            future_steps=[1, 2, 4, 8],
+        )
+    }
+
+    observation_components = {
+        "noisy_reduced_coords_obs": reduced_coords_obs_factory(
+            use_noisy=True,
+            root_height_obs=False,
+            root_vel_obs=False,
+        ),
+        "noisy_mimic_reduced_coords_target_poses": (
+            mimic_target_poses_reduced_coords_factory(
+                use_noisy=True,
+                include_dof_vel=True,
+                include_xy_offset=False,
+            )
+        ),
+        "clean_reduced_coords_obs": reduced_coords_obs_factory(
+            use_noisy=False,
+            root_height_obs=False,
+            root_vel_obs=False,
+        ),
+        "clean_mimic_reduced_coords_target_poses": (
+            mimic_target_poses_reduced_coords_factory(
+                use_noisy=False,
+                include_dof_vel=True,
+                include_xy_offset=False,
+            )
+        ),
+        "max_coords_obs": max_coords_obs_factory(
+            use_noisy=False,
+            local_obs=True,
+            root_height_obs=True,
+            observe_contacts=False,
+        ),
+        "mimic_max_coords_target_poses": mimic_target_poses_max_coords_factory(
+            use_noisy=False,
+            with_velocities=True,
+            with_relative=True,
+        ),
+        "historical_previous_processed_actions": previous_actions_factory(
+            history_steps=1, processed=True
+        ),
+        "text_embedding_obs": MdpComponent(
+            compute_func=passthrough_text_embedding,
+            dynamic_vars={
+                "text_embedding": EnvContext.mimic.text_embedding,
+            },
+        ),
+    }
+
+    expert_model_path = getattr(args, "expert_model_path", None)
+    if expert_model_path:
+        from protomotions.agents.distill.utils import (
+            get_expert_observation_components,
+            load_expert_configs,
+        )
+
+        expert_configs = load_expert_configs(expert_model_path)
+        expert_obs_components = get_expert_observation_components(
+            expert_configs["env"],
+            expert_configs["agent"],
+            existing_obs_keys=list(observation_components.keys()),
+        )
+        observation_components.update(expert_obs_components)
+
+    reward_components = {
+        "global_anchor_ori": global_anchor_ori_rew_factory(weight=0.5, sigma=0.4),
+        "relative_body_pos": relative_body_pos_rew_factory(
+            weight=1.0,
+            sigma=0.3,
+            use_region_weights=True,
+        ),
+        "relative_body_ori": relative_body_ori_rew_factory(
+            weight=1.0,
+            sigma=0.4,
+            use_region_weights=True,
+        ),
+        "body_lin_vel": global_body_lin_vel_rew_factory(
+            weight=1.0,
+            sigma=1.0,
+            use_region_weights=True,
+        ),
+        "body_ang_vel": global_body_ang_vel_rew_factory(
+            weight=1.0,
+            sigma=3.14,
+            use_region_weights=True,
+        ),
+        "action_rate": action_smoothness_factory(weight=-0.1),
+        "limits_dof_pos": MdpComponent(
+            compute_func=compute_soft_pos_limit_rew,
+            dynamic_vars={"dof_pos": EnvContext.current.dof_pos},
+            static_params={
+                "weight": -10.0,
+                "dof_limits_lower": robot_cfg.kinematic_info.dof_limits_lower,
+                "dof_limits_upper": robot_cfg.kinematic_info.dof_limits_upper,
+            },
+        ),
+    }
+
+    return EnvConfig(
+        ref_contact_smooth_window=7,
+        max_episode_length=1000,
+        num_state_history_steps=1,
+        control_components=control_components,
+        observation_components=observation_components,
+        termination_components={
+            "fall": anchor_height_error_term_factory(threshold=0.25),
+        },
+        reward_components=reward_components,
+        action_config=make_bm_pd_action_config(robot_cfg),
+        motion_manager=MimicMotionManagerConfig(
+            init_start_prob=0.2,
+            resample_on_reset=True,
+            realign_motion_with_humanoid_on_each_step=False,
+        ),
+    )
+
+
+def agent_config(
+    robot_config: RobotConfig, env_config: EnvConfig, args: argparse.Namespace
+) -> DistillAgentConfig:
+    from protomotions.agents.base_agent.config import OptimizerConfig
+    from protomotions.agents.common.config import (
+        MLPWithConcatConfig,
+        MLPLayerConfig,
+        ModuleContainerConfig,
+        ModuleOperationForwardConfig,
+        ObsProcessorConfig,
+    )
+    from protomotions.agents.evaluators.config import (
+        DistillEvaluatorConfig,
+        MotionWeightsRulesConfig,
+    )
+    from protomotions.envs.component_factories import (
+        anchor_ori_metric_factory,
+        relative_body_pos_metric_factory,
+        anchor_height_error_metric_factory,
+        gt_error_factory,
+        gr_error_factory,
+        max_joint_error_factory,
+    )
+
+    vae_latent_dim = 64
+
+    encoder_config = ModuleContainerConfig(
+        in_keys=[
+            "noisy_reduced_coords_obs",
+            "noisy_mimic_reduced_coords_target_poses",
+            "historical_previous_processed_actions",
+            "text_embedding_obs",
+        ],
+        out_keys=["encoder_mu", "encoder_logvar"],
+        models=[
+            ObsProcessorConfig(
+                in_keys=[
+                    "noisy_reduced_coords_obs",
+                    "noisy_mimic_reduced_coords_target_poses",
+                    "historical_previous_processed_actions",
+                ],
+                out_keys=["encoder_motion_obs_norm"],
+                normalize_obs=True,
+                norm_clamp_value=5,
+                module_operations=[ModuleOperationForwardConfig()],
+            ),
+            MLPWithConcatConfig(
+                in_keys=[
+                    "encoder_motion_obs_norm",
+                    "text_embedding_obs",
+                ],
+                out_keys=["encoder_trunk_out"],
+                num_out=512,
+                layers=[MLPLayerConfig(units=1024, activation="relu") for _ in range(4)],
+                output_activation="relu",
+            ),
+            MLPWithConcatConfig(
+                in_keys=["encoder_trunk_out"],
+                out_keys=["encoder_mu"],
+                num_out=vae_latent_dim,
+                layers=[
+                    MLPLayerConfig(units=256, activation="relu"),
+                    MLPLayerConfig(units=128, activation="relu"),
+                ],
+            ),
+            MLPWithConcatConfig(
+                in_keys=["encoder_trunk_out"],
+                out_keys=["encoder_logvar"],
+                num_out=vae_latent_dim,
+                layers=[
+                    MLPLayerConfig(units=256, activation="relu"),
+                    MLPLayerConfig(units=128, activation="relu"),
+                ],
+            ),
+        ],
+    )
+
+    prior_config = ModuleContainerConfig(
+        in_keys=[
+            "noisy_reduced_coords_obs",
+            "historical_previous_processed_actions",
+            "text_embedding_obs",
+        ],
+        out_keys=["prior_mu", "prior_logvar"],
+        models=[
+            ObsProcessorConfig(
+                in_keys=[
+                    "noisy_reduced_coords_obs",
+                    "historical_previous_processed_actions",
+                ],
+                out_keys=["prior_motion_obs_norm"],
+                normalize_obs=True,
+                norm_clamp_value=5,
+                module_operations=[ModuleOperationForwardConfig()],
+            ),
+            MLPWithConcatConfig(
+                in_keys=[
+                    "prior_motion_obs_norm",
+                    "text_embedding_obs",
+                ],
+                out_keys=["prior_trunk_out"],
+                num_out=512,
+                layers=[MLPLayerConfig(units=1024, activation="relu") for _ in range(4)],
+                output_activation="relu",
+            ),
+            MLPWithConcatConfig(
+                in_keys=["prior_trunk_out"],
+                out_keys=["prior_mu"],
+                num_out=vae_latent_dim,
+                layers=[
+                    MLPLayerConfig(units=256, activation="relu"),
+                    MLPLayerConfig(units=128, activation="relu"),
+                ],
+            ),
+            MLPWithConcatConfig(
+                in_keys=["prior_trunk_out"],
+                out_keys=["prior_logvar"],
+                num_out=vae_latent_dim,
+                layers=[
+                    MLPLayerConfig(units=256, activation="relu"),
+                    MLPLayerConfig(units=128, activation="relu"),
+                ],
+            ),
+        ],
+    )
+
+    trunk_config = ModuleContainerConfig(
+        in_keys=[
+            "noisy_reduced_coords_obs",
+            "historical_previous_processed_actions",
+            "vae_latent",
+        ],
+        out_keys=["actor_trunk_out"],
+        models=[
+            MLPWithConcatConfig(
+                in_keys=[
+                    "noisy_reduced_coords_obs",
+                    "historical_previous_processed_actions",
+                    "vae_latent",
+                ],
+                out_keys=["actor_trunk_out"],
+                normalize_obs=True,
+                norm_clamp_value=5,
+                num_out=robot_config.number_of_actions,
+                layers=[MLPLayerConfig(units=1024, activation="relu") for _ in range(6)],
+                output_activation="tanh",
+            ),
+        ],
+    )
+
+    model_config = DistillModelConfig(
+        _target_="protomotions.agents.distill.model.DetachedEncoderKLDistillModel",
+        encoder=encoder_config,
+        prior=prior_config,
+        trunk=trunk_config,
+        vae=VaeConfig(
+            vae_latent_dim=vae_latent_dim,
+            vae_noise_type=VaeNoiseType.NORMAL,
+            prior_regu_weight=0.005,
+            prior_mean_regu_coeff=0.001,
+            prior_logvar_regu_coeff=0.001,
+            kld_schedule=KLDScheduleConfig(
+                start_epoch=2500,
+                end_epoch=5000,
+                init_kld_coeff=0.01,
+                end_kld_coeff=0.001,
+            ),
+        ),
+        optimizer=OptimizerConfig(_target_="torch.optim.Adam", lr=2e-5),
+    )
+
+    return DistillAgentConfig(
+        model=model_config,
+        batch_size=args.batch_size,
+        training_max_steps=args.training_max_steps,
+        gradient_clip_val=50.0,
+        num_mini_epochs=6,
+        evaluator=DistillEvaluatorConfig(
+            use_privileged_success_for_motion_weights=True,
+            evaluation_components={
+                "anchor_ori": anchor_ori_metric_factory(),
+                "relative_body_pos": relative_body_pos_metric_factory(),
+                "anchor_height_error": anchor_height_error_metric_factory(
+                    threshold=0.25
+                ),
+                "gt_error": gt_error_factory(),
+                "gr_error": gr_error_factory(),
+                "max_joint_error": max_joint_error_factory(),
+            },
+            motion_weights_rules=MotionWeightsRulesConfig(
+                motion_weights_update_success_discount=0.999,
+                motion_weights_update_failure_discount=0,
+            ),
+        ),
+        expert_model_path=getattr(args, "expert_model_path", None),
+    )
+
+
+def configure_robot_and_simulator(
+    robot_cfg: RobotConfig, simulator_cfg: SimulatorConfig, args: argparse.Namespace
+):
+    robot_cfg.update_fields(
+        contact_bodies=["all_left_foot_bodies", "all_right_foot_bodies"]
+    )
+
+    robot_cfg.reset_noise = RobotNoiseConfig(
+        dof_pos_noise=0.1,
+        root_pos_noise=[0.05, 0.05, 0.01],
+        root_rot_noise=[0.1, 0.1, 0.2],
+        root_vel_noise=[0.1, 0.1, 0.05],
+        root_ang_vel_noise=[0.1, 0.1, 0.1],
+    )
+
+    simulator_cfg.domain_randomization = DomainRandomizationConfig(
+        action_noise=ActionNoiseDomainRandomizationConfig(
+            action_noise_range=(-0.025, 0.025), dof_names=[".*"], dof_indices=None
+        ),
+        friction=FrictionDomainRandomizationConfig(
+            num_buckets=64,
+            static_friction_range=(0.3, 1.6),
+            dynamic_friction_range=(0.3, 1.2),
+            restitution_range=(0.0, 0.5),
+            body_names=[".*"],
+            body_indices=None,
+        ),
+        center_of_mass=CenterOfMassDomainRandomizationConfig(
+            com_range={"x": (-0.025, 0.025), "y": (-0.05, 0.05), "z": (-0.05, 0.05)},
+            body_names=robot_cfg.common_naming_to_robot_body_names["torso_body_name"],
+            body_indices=None,
+        ),
+        observation_noise=RobotNoiseConfig(
+            dof_pos_noise=0.01,
+            dof_vel_noise=0.5,
+            anchor_ang_vel_noise=0.2,
+            anchor_rot_noise=0.05,
+        ),
+        push=PushDomainRandomizationConfig(
+            push_interval_range=(1.0, 3.0),
+            max_linear_velocity=(0.5, 0.5, 0.2),
+            max_angular_velocity=(0.52, 0.52, 0.78),
+        ),
+    )
+
+
+def apply_inference_overrides(
+    robot_cfg: RobotConfig,
+    simulator_cfg: SimulatorConfig,
+    env_cfg: EnvConfig,
+    agent_cfg: DistillAgentConfig,
+    terrain_cfg: TerrainConfig,
+    motion_lib_cfg: MotionLibConfig,
+    scene_lib_cfg: SceneLibConfig,
+    args: argparse.Namespace,
+):
+    from protomotions.envs.component_factories import (
+        reduced_coords_obs_factory,
+        mimic_target_poses_reduced_coords_factory,
+    )
+
+    if hasattr(env_cfg, "termination_components") and env_cfg.termination_components:
+        env_cfg.termination_components = {}
+
+    env_cfg.max_episode_length = 1000000
+    env_cfg.motion_manager.resample_on_reset = True
+    env_cfg.motion_manager.init_start_prob = 1.0
+
+    if agent_cfg is not None and hasattr(agent_cfg, "expert_model_path"):
+        agent_cfg.expert_model_path = None
+
+    terrain_cfg.sim_config = TerrainSimConfig(
+        static_friction=1.0,
+        dynamic_friction=1.0,
+        restitution=0.0,
+        combine_mode=CombineMode.AVERAGE,
+    )
+    simulator_cfg.domain_randomization = None
+
+    env_cfg.observation_components["noisy_reduced_coords_obs"] = (
+        reduced_coords_obs_factory(
+            use_noisy=False,
+            root_height_obs=False,
+            root_vel_obs=False,
+        )
+    )
+    env_cfg.observation_components["noisy_mimic_reduced_coords_target_poses"] = (
+        mimic_target_poses_reduced_coords_factory(
+            use_noisy=False,
+            include_dof_vel=True,
+            include_xy_offset=False,
+        )
+    )
